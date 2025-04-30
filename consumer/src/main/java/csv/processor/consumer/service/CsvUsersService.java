251370effd9e6cb8d7ec04b18aed09654a34fc7f
romulomdr <romulomatheus31@gmail.com>
2025-04-30 20:39:38 -0300

    start create CsvUsersService in consumer

--- consumer/src/main/java/csv/processor/consumer/service/CsvUsersService.java
+++ consumer/src/main/java/csv/processor/consumer/service/CsvUsersService.java
@@ -1,9 +1,22 @@
 package csv.processor.consumer.service;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CsvUsersService {
 
+	private RabbitTemplate rabbitTemplate;
+
+	public CsvUsersService(RabbitTemplate rabbitTemplate) {
+		this.rabbitTemplate = rabbitTemplate;
+	}
+	
+	public void consumeQueue() {
+	}
+	
+	public void saveUsers() {
+		
+	}
 	
 }
